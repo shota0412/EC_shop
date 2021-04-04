47a3bf9efc89a8cad68168a3632288802dda77c6
shota0412 <sh.v.w0412@gmail.com>
2021-04-05 00:42:42 +0900

    CarrierWaveの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+
+  mount_uploader :image, ImageUploader
 end
